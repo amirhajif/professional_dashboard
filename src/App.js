6cdd2172106d6a7de7ce1488ac2be5bf2056f619
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-01-27 20:48:47 +0330

    ADD sidebar component

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div>
+          </div> 
           <div>
             <Routes>
               {/*  Dashboard */}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 
-const Sidebar = () => {
+import {Link , NavLink} from 'react-router-dom';
+import {SiShopware} from 'react-icons/si'
+import {MdOutlineCancel} from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import {links} from '../data/dummy'
+ 
+const  Sidebar = () => {
+  const activeMenu =true;
   return (
-    <div>Sidebar</div>
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+      {activeMenu && (<>
+        <div className='flex justify-between items-center'>
+          <Link to='/' onClick={()=>{}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight 
+          dark:text-white text-slate-900  '>
+            <SiShopware /><span>Shoppy</span>
+          </Link>
+          <TooltipComponent content="Menu" position="BottomCenter">
+            <button type='button' onClick={()=>{}}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        <div className='mt-10 '>
+          {links.map((item) =>(
+            <div key={item.title} >
+              <p className='text-gray-400 m-3 mt-4 uppercase'>
+              {item.title}
+              </p>
+              {item.links.map((link) =>(
+                <NavLink to={`/${link.name}`}
+                  key={link.name}
+                  onClick={()=>{}}
+                >
+                  
+                </NavLink>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>)}
+    </div>
+
   )
 }
 
