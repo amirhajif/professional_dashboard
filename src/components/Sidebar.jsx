d50eaeb0f7decf80947bfb5b4a3958cbf3efcb74
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-01-28 01:33:16 +0330

    Complete Sidebar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,8 @@ import {links} from '../data/dummy'
  
 const  Sidebar = () => {
   const activeMenu =true;
+  const activeLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const normalLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
@@ -35,8 +37,12 @@ const  Sidebar = () => {
                 <NavLink to={`/${link.name}`}
                   key={link.name}
                   onClick={()=>{}}
+                  className={({isActive})=>isActive?activeLink:normalLink}
                 >
-                  
+                  {link.icon}
+                  <span className='capitalize'>
+                    {link.name}
+                  </span>
                 </NavLink>
               ))}
             </div>
