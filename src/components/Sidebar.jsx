17091178dfc21b8430c64f4db535861a8e897f4a
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-01-31 17:01:45 +0330

    hide sidebar in Mobile mod

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,14 @@ import {links} from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider';
  
 const  Sidebar = () => {
-  const {activeMenu,setActiveMenu} =useStateContext();
+  const {activeMenu,setActiveMenu,screenSize} =useStateContext();
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize>=900){
+      setActiveMenu(false);
+    }
+  }
+
   const activeLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
