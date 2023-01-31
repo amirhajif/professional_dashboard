a7aa3aa704fe1f96fe158bacb3d34ce2bb2db4a7
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-01-31 17:46:03 +0330

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,9 @@ const NavButton=({title , customFunc , icon , color ,dotColor})=>(
   <TooltipComponent content={title} position="BottomCenter">
     <button type='button' onClick={customFunc} style={{color}} 
     className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-      <span style={{baclgorund:dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+      <span style={{baclgorund:dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
         {icon}
-      </span>
+      {/* </span> */}
     </button>
   </TooltipComponent>
 )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const  Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={()=>setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight 
+          <Link to='/' onClick={handleCloseSidebar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight 
           dark:text-white text-slate-900  '>
             <SiShopware /><span>Shoppy</span>
           </Link>
@@ -44,7 +44,7 @@ const  Sidebar = () => {
               {item.links.map((link) =>(
                 <NavLink to={`/${link.name}`}
                   key={link.name}
-                  onClick={()=>{}}
+                  onClick={handleCloseSidebar}
                   className={({isActive})=>isActive?activeLink:normalLink}
                 >
                   {link.icon}
