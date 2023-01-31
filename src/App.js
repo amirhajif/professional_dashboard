11c25dd6fd3f895a896b8b79a5c052696e337682
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-01-31 18:17:27 +0330

    ADD button Component

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div> 
+          
           <div>
             <Routes>
               {/*  Dashboard */}
@@ -65,6 +65,7 @@ const App = () => {
 
             </Routes>
           </div>
+          </div> 
         </div>
       </BrowserRouter>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({bgColor,color,size,text,borderRadius}) => {
   return (
-    <div>Button</div>
+    <button type='button'
+      style={{backgroundColor:bgColor,color:color,borderRadius:borderRadius}}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
   )
 }
  
