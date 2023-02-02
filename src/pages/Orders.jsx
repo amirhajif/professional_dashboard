9e150ee54291b6c3e7b95a0410fde3101e576f74
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-02-02 12:45:23 +0330

    Complete Orders Page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -8,10 +8,20 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        // editSettings={editing}
+      >
       <ColumnsDirective>
           {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-        </ColumnsDirective>
+      </ColumnsDirective>
+      <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
     </div>
   )
