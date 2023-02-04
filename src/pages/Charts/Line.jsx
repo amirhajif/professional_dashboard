2d5f50247f9550709bb1027b85d0b3495cbbb4da
amirhossein hajitabar <amirhajitabar2@gmail.com>
2023-02-04 23:40:12 +0330

    Implementing Line Component and Page

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Line = () => {
-  return (
-    <div>
-      
+import { ChartsHeader, LineChart } from '../../components';
+
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
     </div>
-  )
-}
+  </div>
+);
 
-export default Line
+export default Line;
